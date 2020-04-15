040be8668a9e894b2700a063338445d03b6a3dac
Connor Dillon <connor@codewithconnor.com>
2020-04-15 15:11:30 -0700

    Built functionality to add form submissions to API, show alert on submit

--- src/app.js
+++ src/app.js
@@ -3,13 +3,42 @@ import { ui } from './ui'
 
 let PORT = 3000
 let server = `http://localhost:${PORT}`
+let fileName = '/posts' // The name (or filepath & name) of the file on the server containing our desired resource
 
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts)
 
+// Listen for submit post
+document.querySelector('.post-submit').addEventListener('click', submitPost)
+
+// GET posts from server
 function getPosts() {
   http
-    .get(server + '/posts')
+    .get(server + fileName)
     .then((data) => ui.showPosts(data))
     .catch((err) => console.log(err))
+}
+
+// Adds post to server
+function submitPost() {
+  const title = document.querySelector('#title').value
+  const body = document.querySelector('#body').value
+
+  const data = {
+    title,
+    body,
+  }
+
+  // Create POST request
+  http
+    .post(server + fileName, data)
+    .then((data) => {
+      ui.showAlert('Post added', 'alert alert-success')
+      ui.clearFields()
+      getPosts()
+      //   console.log('App Test - Submit Post: ', getPosts())
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
--- src/ui.js
+++ src/ui.js
@@ -1,8 +1,8 @@
 class UI {
   constructor() {
     this.post = document.querySelector('#posts')
-    this.title = document.querySelector('#title')
-    this.body = document.querySelector('#body')
+    this.titleInput = document.querySelector('#title')
+    this.bodyInput = document.querySelector('#body')
     this.idInput = document.querySelector('#id')
     this.postSubmit = document.querySelector('#post-submit')
     this.forState = 'add'
@@ -27,6 +27,44 @@ class UI {
     })
 
     this.post.innerHTML = output
+  }
+
+  showAlert(message, className) {
+    this.clearAlert()
+
+    // Create div
+    const div = document.createElement('div')
+    // Add classes
+    div.className = className
+    // Add text
+    div.appendChild(document.createTextNode(message))
+
+    // Get parent element
+    const container = document.querySelector('.postsContainer')
+
+    // Get posts
+    const posts = document.querySelector('#posts')
+
+    // Insert alert div
+    container.insertBefore(div, posts)
+
+    // Timeout
+    setTimeout(() => {
+      this.clearAlert()
+    }, 2000)
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert')
+
+    if (currentAlert) {
+      currentAlert.remove()
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = ''
+    this.bodyInput.value = ''
   }
 }
 
